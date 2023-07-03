da5dcdb9c131f2553876df4ef79f20a14c8d188b
amos zohar <azamoszohar@gmail.com>
2023-07-03 16:45:11 +0300

    finished 1st ex

--- Ex7.c
+++ Ex7.c
@@ -2,7 +2,27 @@
 
 int FindCommonDigit(unsigned long n1, unsigned long n2)
 {
-
+	/*
+	* I will solve with backtracking
+	*/
+	/*
+	* if n1==0==n2, means we exhausted all options, yet found no matching digits.
+	* return -1;
+	*/
+	if (n1 == 0 || n2 == 0) {
+		return -1;
+	}
+	int digit = n1 % 10;
+	if (n2 % 10 == digit) {
+		return digit;
+	}
+	int res1 = FindCommonDigit(n1 / 10, n2);
+	int res2 = FindCommonDigit(n1, n2 / 10);
+	/*
+	* Now, since we know that the numbers are ordered,
+	* in case of more then 1 match, returning the larger digit will yield the rightmost equal digit
+	*/
+	return max(res1, res2);
 }
 
 
